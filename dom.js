0a431ef4176b17b9307b4b08a7c17a50fc5119d5
Kevin Smith <123609428+ksmith552@users.noreply.github.com>
2023-05-09 00:43:12 -0500

    intial commits

---
+++ dom.js
@@ -0,0 +1,9 @@
+// grid-container styling
+
+// create grid template 16x16 using flexbox - 16 divs with 16 children eachg
+
+// create an event listener on all divs for hover that calls the drawing function. possible array?
+
+// event listener for grid size
+
+// event listener for board reset
---
+++ script.js
@@ -0,0 +1,5 @@
+// create a function that changes the color of a div to BLACK when hovered on
+
+// create a function that will prompt user input for a grid size selection *100 limit*
+
+// create a function that resets the board to default value
