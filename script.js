66cfde8e9aeeee8bde8784afcabfd0aece3b1ed7
Kevin Smith <123609428+ksmith552@users.noreply.github.com>
2023-05-17 23:17:08 -0500

    add grid selection
    created a function that prompts the uses for a selection, then uses that
    selection to create a new grid with that parameter

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+const gridDefault = 16;
+let gridSelection;
+
 // create a function that creates grid sqaures
 
 /**
  * This function calls createGridRow() 16 times, creating the full grid
  */
 function createGrid() {
-  for (let i = 0; i < 25; i++) {
+  for (let i = 0; i < gridSelection || i < gridDefault; i++) {
     createGridRow();
   }
 }
@@ -16,9 +19,9 @@ function createGridRow() {
   const parentRow = document.createElement('div');
   parentRow.classList.add('parent-row');
 
-  grid.appendChild(parentRow);
+  grid.appendChild(parentRow); // / this function is hugggee. make this better
 
-  for (let i = 0; i < 25; i++) {
+  for (let i = 0; i < gridSelection || i < gridDefault; i++) {
     const rowChildren = document.createElement('div');
     rowChildren.classList.add('row-children');
     rowChildren.style.borderStyle = 'double';
@@ -30,6 +33,27 @@ function createGridRow() {
   }
 }
 
+// create a function that will prompt user input for a grid size selection *100 limit*
+/**
+ * This invokes when the rest button is pressed
+ * the prompt takes input from the user, removes the previous grid,
+ * and creates the new user defined grid
+ */
+function gridSize() {
+  gridSelection = prompt(
+      'Input the desired amount of sqaures per side',
+      'type a number between 16 - 100',
+  );
+  grid.replaceChildren();
+  if (gridSelection <= 100) {
+    // 16 grid squares minimum, fix this
+    // add any input that is not an integer of 1-100 as null/undefined
+    createGrid(gridSelection);
+  } else {
+    createGrid(gridDefault);
+    alert('Error, please input a valid number in between 1-100');
+  }
+}
 // create a function that will prompt user input for a grid size selection *100 limit*
 
 // create a function that changes the color of a div to BLACK when hovered on
