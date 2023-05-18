e64365b7b7125e7af604fd54e3d4ddb733d6237f
Kevin Smith <123609428+ksmith552@users.noreply.github.com>
2023-05-17 23:15:50 -0500

    Add event listener
    added event listener for grid selection on click that invokes the gridSize
    function

--- dom.js
+++ dom.js
@@ -2,18 +2,23 @@
 
 const grid = document.getElementById('grid');
 
-// controls
+// Controls
 const controls = document.querySelector('#controls');
+
+// Grid Selection
 const gridButton = document.createElement('button');
 gridButton.classList.add('buttons');
+gridButton.addEventListener('click', gridSize);
 gridButton.innerText = 'Select Grid Size';
 controls.appendChild(gridButton);
 
+// Reset grid
 const resetButton = document.createElement('button');
 resetButton.classList.add('buttons');
 resetButton.innerText = 'Reset SketchPad';
 controls.appendChild(resetButton);
 
+// Change color
 const colorButton = document.createElement('button');
 colorButton.classList.add('buttons');
 colorButton.innerText = 'Choose Color';
